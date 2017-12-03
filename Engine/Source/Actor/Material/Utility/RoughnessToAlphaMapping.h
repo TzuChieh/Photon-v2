e8029377ebb4d25e77e243307397b325ba1369de
Tzu-Chieh Chang <b01502051@ntu.edu.tw>
2017-12-03 22:16:36 +0800

    [refactor] better comment

--- Engine/Source/Actor/Material/Utility/RoughnessToAlphaMapping.h
+++ Engine/Source/Actor/Material/Utility/RoughnessToAlphaMapping.h
@@ -10,6 +10,17 @@ namespace ph
 	
 class RoughnessToAlphaMapping final
 {
+	// Maps roughness value in [0, 1] to alpha parameter used by many 
+	// microfacet distribution functions. Generally, the mapping can be quite
+	// beneficial for artists since directly consider roughness as alpha value 
+	// often does not result in linearly varying specular highlight across
+	// equally spaced roughness values.
+	//
+	// Strangely, the original paper: 
+	// Microfacet Models for Refraction through Rough Surfaces by Walter et al. 
+	// does not include such mapping for GGX distribution, only the ones for 
+	// other kinds of distribution.
+
 public:
 
 	static inline real equaled(const real roughness)
@@ -28,10 +39,6 @@ public:
 	}
 
 	// This mapping is used in PBRT-v3.
-	// Strangely the original paper: 
-	// Microfacet Models for Refraction through Rough Surfaces by Walter et al. 
-	// does not include such mapping for GGX distribution, only the ones for 
-	// other kinds of distribution.
 	static inline real pbrtV3(const real roughness)
 	{
 		const real clampedRoughness = std::max(roughness, 0.001_r);
