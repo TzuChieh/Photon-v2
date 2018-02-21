671ba47445f81ae35f1fde86f757f0b847bf09ae
Tzu-Chieh Chang <b01502051@ntu.edu.tw>
2018-02-21 21:38:07 +0800

    data for Smits rgb to spectrum algorithm

---
+++ Engine/Source/Core/Quantity/Private/spectral_data.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Common/primitive_type.h"
+
+#include <array>
+#include <cstddef>
+
+namespace ph
+{
+
+namespace spectral_data
+{
+
+typedef std::array<real, 64> Array64R;
+
+namespace reflectance
+{
+
+const Array64R& smits_linear_sRGB_to_spectrum_wavelengths_nm();
+const Array64R& smits_linear_sRGB_to_spectrum_white();
+const Array64R& smits_linear_sRGB_to_spectrum_cyan();
+const Array64R& smits_linear_sRGB_to_spectrum_magenta();
+const Array64R& smits_linear_sRGB_to_spectrum_yellow();
+const Array64R& smits_linear_sRGB_to_spectrum_red();
+const Array64R& smits_linear_sRGB_to_spectrum_green();
+const Array64R& smits_linear_sRGB_to_spectrum_blue();
+
+}// end namespace reflectance
+
+namespace emission
+{
+
+const Array64R& smits_linear_sRGB_to_spectrum_wavelengths_nm();
+const Array64R& smits_linear_sRGB_to_spectrum_white();
+const Array64R& smits_linear_sRGB_to_spectrum_cyan();
+const Array64R& smits_linear_sRGB_to_spectrum_magenta();
+const Array64R& smits_linear_sRGB_to_spectrum_yellow();
+const Array64R& smits_linear_sRGB_to_spectrum_red();
+const Array64R& smits_linear_sRGB_to_spectrum_green();
+const Array64R& smits_linear_sRGB_to_spectrum_blue();
+
+}// end namespace emission
+
+}// end namespace spectral_data
+
+}// end namespace ph
