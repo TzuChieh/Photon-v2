4bef535a3da0c2148666e031ffb18dbd535a2ab5
Tzu-Chieh Chang <b01502051@ntu.edu.tw>
2018-02-23 19:18:12 +0800

    CIE D65 spectrum data

--- Engine/Source/Core/Quantity/ColorSpace/spectral_data.h
+++ Engine/Source/Core/Quantity/ColorSpace/spectral_data.h
@@ -13,12 +13,17 @@ namespace spectral_data
 
 typedef std::array<real, 40>  ArraySmits;
 typedef std::array<real, 471> ArrayXYZCMF;
+typedef std::array<real, 531> ArrayD65;
 
 // XYZ color matching functions for CIE 1931 2-degree standard.
 const ArrayXYZCMF& XYZ_CMF_CIE_1931_2_degree_wavelengths_nm();
 const ArrayXYZCMF& XYZ_CMF_CIE_1931_2_degree_X();
 const ArrayXYZCMF& XYZ_CMF_CIE_1931_2_degree_Y();
 const ArrayXYZCMF& XYZ_CMF_CIE_1931_2_degree_Z();
+
+// CIE standard D65 illuminant spectrum.
+const ArrayD65& CIE_D65_wavelengths_nm();
+const ArrayD65& CIE_D65_values();
 
 // Data for Smits' RGB to spectrum conversion algorithm.
 // The conversion result will resembles a standard illuminant E's spectrum 
