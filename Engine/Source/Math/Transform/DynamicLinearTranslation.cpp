b206810b59c634f209ad7dea0180cc87eacfa662
T.C. Chang <b01502051@ntu.edu.tw>
2018-03-14 15:16:20 +0800

    DynamicLinearTranslation now is-a RigidTransform

--- Engine/Source/Math/Transform/DynamicLinearTranslation.cpp
+++ Engine/Source/Math/Transform/DynamicLinearTranslation.cpp
@@ -8,9 +8,7 @@ DynamicLinearTranslation::DynamicLinearTranslation(const Vector3R& translationT0
                                                    const Vector3R& translationT1) :
 	m_translationT0(translationT0), 
 	m_translationT1(translationT1)
-{
-
-}
+{}
 
 DynamicLinearTranslation::~DynamicLinearTranslation() = default;
 
@@ -20,36 +18,40 @@ std::unique_ptr<Transform> DynamicLinearTranslation::genInversed() const
 	                                                  m_translationT1.mul(-1));
 }
 
-void DynamicLinearTranslation::transformVector(const Vector3R& vector, 
-                                               const Time&     time,
-                                               Vector3R* const out_vector) const
+void DynamicLinearTranslation::transformVector(
+	const Vector3R& vector, 
+	const Time&     time,
+	Vector3R* const out_vector) const
 {
 	*out_vector = vector;
 }
 
-void DynamicLinearTranslation::transformOrientation(const Vector3R& orientation, 
-                                                    const Time&     time,
-                                                    Vector3R* const out_orientation) const
+void DynamicLinearTranslation::transformOrientation(
+	const Vector3R& orientation, 
+	const Time&     time,
+	Vector3R* const out_orientation) const
 {
 	*out_orientation = orientation;
 }
 
-void DynamicLinearTranslation::transformPoint(const Vector3R& point, 
-                                              const Time&     time,
-                                              Vector3R* const out_point) const
+void DynamicLinearTranslation::transformPoint(
+	const Vector3R& point, 
+	const Time&     time,
+	Vector3R* const out_point) const
 {
 	*out_point = point.add(Vector3R::lerp(m_translationT0, m_translationT1, time.relativeT));
 }
 
-void DynamicLinearTranslation::transformLineSegment(const Vector3R& lineStartPos, 
-                                                    const Vector3R& lineDir,
-                                                    const real      lineMinT, 
-                                                    const real      lineMaxT,
-                                                    const Time&     time,
-                                                    Vector3R* const out_lineStartPos, 
-                                                    Vector3R* const out_lineDir,
-                                                    real* const     out_lineMinT, 
-                                                    real* const     out_lineMaxT) const
+void DynamicLinearTranslation::transformLineSegment(
+	const Vector3R& lineStartPos, 
+	const Vector3R& lineDir,
+	const real      lineMinT, 
+	const real      lineMaxT,
+	const Time&     time,
+	Vector3R* const out_lineStartPos, 
+	Vector3R* const out_lineDir,
+	real* const     out_lineMinT, 
+	real* const     out_lineMaxT) const
 {
 	DynamicLinearTranslation::transformPoint(lineStartPos, time, out_lineStartPos);
 	*out_lineDir  = lineDir;
--- Engine/Source/Math/Transform/DynamicLinearTranslation.h
+++ Engine/Source/Math/Transform/DynamicLinearTranslation.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "Math/Transform/Transform.h"
+#include "Math/Transform/RigidTransform.h"
 #include "Math/TVector3.h"
 
 namespace ph
 {
 
-class DynamicLinearTranslation : public Transform
+class DynamicLinearTranslation : public RigidTransform
 {
 public:
 	DynamicLinearTranslation(const Vector3R& translationT0, 
@@ -16,19 +16,31 @@ public:
 	virtual std::unique_ptr<Transform> genInversed() const override;
 
 private:
-	virtual void transformVector(const Vector3R& vector, const Time& time, 
-	                             Vector3R* out_vector) const override;
+	virtual void transformVector(
+		const Vector3R& vector,
+		const Time&     time,
+		Vector3R*       out_vector) const override;
 
-	virtual void transformOrientation(const Vector3R& orientation, const Time& time,
-	                                  Vector3R* out_orientation) const override;
+	virtual void transformOrientation(
+		const Vector3R& orientation,
+		const Time&     time,
+		Vector3R*       out_orientation) const override;
 
-	virtual void transformPoint(const Vector3R& point, const Time& time, 
-	                            Vector3R* out_point) const override;
+	virtual void transformPoint(
+		const Vector3R& point,
+		const Time&     time,
+		Vector3R*       out_point) const override;
 
-	virtual void transformLineSegment(const Vector3R& lineStartPos, const Vector3R& lineDir, 
-	                                  real lineMinT, real lineMaxT, const Time& time, 
-	                                  Vector3R* out_lineStartPos, Vector3R* out_lineDir,
-	                                  real* out_lineMinT, real* out_lineMaxT) const override;
+	virtual void transformLineSegment(
+		const Vector3R& lineStartPos,
+		const Vector3R& lineDir,
+		real            lineMinT,
+		real            lineMaxT,
+		const Time&     time,
+		Vector3R*       out_lineStartPos,
+		Vector3R*       out_lineDir,
+		real*           out_lineMinT,
+		real*           out_lineMaxT) const override;
 
 	Vector3R m_translationT0;
 	Vector3R m_translationT1;
