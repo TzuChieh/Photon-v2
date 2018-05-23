3cd9c0ae09b5b916c365a5ea523c5a98e352503e
T.C. Chang <b01502051@ntu.edu.tw>
2018-05-23 15:10:17 +0800

    [bugfix] matrix inversion and filling

--- EngineTest/Source/Math/TMatrix3_test.cpp
+++ EngineTest/Source/Math/TMatrix3_test.cpp
@@ -4,19 +4,40 @@
 
 typedef ph::TMatrix3<float> Matrix;
 
+namespace
+{
+	void expect_all_zero(const Matrix& matrix)
+	{
+		EXPECT_EQ(matrix.m[0][0], 0.0f);
+		EXPECT_EQ(matrix.m[0][1], 0.0f);
+		EXPECT_EQ(matrix.m[0][2], 0.0f);
+		EXPECT_EQ(matrix.m[1][0], 0.0f);
+		EXPECT_EQ(matrix.m[1][1], 0.0f);
+		EXPECT_EQ(matrix.m[1][2], 0.0f);
+		EXPECT_EQ(matrix.m[2][0], 0.0f);
+		EXPECT_EQ(matrix.m[2][1], 0.0f);
+		EXPECT_EQ(matrix.m[2][2], 0.0f);
+	}
+
+	void expect_identity(const Matrix& matrix)
+	{
+		EXPECT_EQ(matrix.m[0][0], 1.0f);
+		EXPECT_EQ(matrix.m[0][1], 0.0f);
+		EXPECT_EQ(matrix.m[0][2], 0.0f);
+		EXPECT_EQ(matrix.m[1][0], 0.0f);
+		EXPECT_EQ(matrix.m[1][1], 1.0f);
+		EXPECT_EQ(matrix.m[1][2], 0.0f);
+		EXPECT_EQ(matrix.m[2][0], 0.0f);
+		EXPECT_EQ(matrix.m[2][1], 0.0f);
+		EXPECT_EQ(matrix.m[2][2], 1.0f);
+	}
+}
+
 TEST(TMatrix3Test, Construction)
 {
 	Matrix mat1;
 
-	EXPECT_EQ(mat1.m[0][0], 0.0f);
-	EXPECT_EQ(mat1.m[0][1], 0.0f);
-	EXPECT_EQ(mat1.m[0][2], 0.0f);
-	EXPECT_EQ(mat1.m[1][0], 0.0f);
-	EXPECT_EQ(mat1.m[1][1], 0.0f);
-	EXPECT_EQ(mat1.m[1][2], 0.0f);
-	EXPECT_EQ(mat1.m[2][0], 0.0f);
-	EXPECT_EQ(mat1.m[2][1], 0.0f);
-	EXPECT_EQ(mat1.m[2][2], 0.0f);
+	expect_all_zero(mat1);
 
 	Matrix mat2(Matrix::Elements{{
 		{1.0f, 2.0f, 3.0f},
@@ -33,4 +54,79 @@ TEST(TMatrix3Test, Construction)
 	EXPECT_EQ(mat2.m[2][0], 7.0f);
 	EXPECT_EQ(mat2.m[2][1], 8.0f);
 	EXPECT_EQ(mat2.m[2][2], 9.0f);
+}
+
+TEST(TMatrix3Test, Multiplying)
+{
+	Matrix mat1(Matrix::Elements{{
+		{1.0f, 2.0f, 3.0f},
+		{4.0f, 5.0f, 6.0f},
+		{7.0f, 8.0f, 9.0f}
+	}});
+
+	Matrix mat2( 0.0f);
+	Matrix mat3(-1.0f);
+
+	Matrix mat4 = mat1.mul(mat2);
+	Matrix mat5 = mat1.mul(mat3);
+
+	expect_all_zero(mat4);
+
+	EXPECT_EQ(mat5.m[0][0], -6.0f);
+	EXPECT_EQ(mat5.m[0][1], -6.0f);
+	EXPECT_EQ(mat5.m[0][2], -6.0f);
+	EXPECT_EQ(mat5.m[1][0], -15.0f);
+	EXPECT_EQ(mat5.m[1][1], -15.0f);
+	EXPECT_EQ(mat5.m[1][2], -15.0f);
+	EXPECT_EQ(mat5.m[2][0], -24.0f);
+	EXPECT_EQ(mat5.m[2][1], -24.0f);
+	EXPECT_EQ(mat5.m[2][2], -24.0f);
+}
+
+TEST(TMatrix3Test, CalcDeterminant)
+{
+	Matrix mat1(Matrix::Elements{{
+		{1.0f, 2.0f, 3.0f},
+		{4.0f, 5.0f, 6.0f},
+		{7.0f, 8.0f, 9.0f}
+	}});
+
+	EXPECT_EQ(mat1.determinant(), 0.0f);
+
+	Matrix mat2(Matrix::Elements{{
+		{1.0f, 0.0f, 0.0f},
+		{0.0f, 1.0f, 0.0f},
+		{0.0f, 0.0f, 1.0f}
+	}});
+	
+	EXPECT_EQ(mat2.determinant(), 1.0f);
+}
+
+TEST(TMatrix3Test, Inversing)
+{
+	Matrix mat1(Matrix::Elements{{
+		{1.0f, 0.0f, 0.0f},
+		{0.0f, 1.0f, 0.0f},
+		{0.0f, 0.0f, 1.0f}
+	}});
+
+	expect_identity(mat1.inverse());
+
+	Matrix mat2(Matrix::Elements{{
+		{-1.0f,  2.0f,  3.0f},
+		{ 1.0f, -2.0f,  3.0f},
+		{ 1.0f,  2.0f, -3.0f}
+	}});
+
+	mat2 = mat2.inverse();
+
+	EXPECT_EQ(mat2.m[0][0], 0.0f);
+	EXPECT_EQ(mat2.m[0][1], 0.5f);
+	EXPECT_EQ(mat2.m[0][2], 0.5f);
+	EXPECT_EQ(mat2.m[1][0], 0.25f);
+	EXPECT_EQ(mat2.m[1][1], 0.0f);
+	EXPECT_EQ(mat2.m[1][2], 0.25f);
+	EXPECT_EQ(mat2.m[2][0], 0.16666666666666666f);
+	EXPECT_EQ(mat2.m[2][1], 0.16666666666666666f);
+	EXPECT_EQ(mat2.m[2][2], 0.0f);
 }
