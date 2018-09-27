cac709104562836b643735bc921035e8ea553da1
Tzu-Chieh Chang <b01502051@ntu.edu.tw>
2018-09-27 06:38:27 -0700

    slightly modified AABB3D

--- Engine/Source/Core/Bound/AABB3D.h
+++ Engine/Source/Core/Bound/AABB3D.h
@@ -24,59 +24,23 @@ public:
 	bool isIntersectingVolume(const Ray& ray, real* out_rayNearHitT, real* out_rayFarHitT) const;
 	bool isIntersectingVolume(const AABB3D& other) const;
 	bool isPoint() const;
+	bool isFiniteVolume() const;
 	AABB3D& unionWith(const AABB3D& other);
 	AABB3D& unionWith(const Vector3R& point);
 
 	// FIXME: too slow
 	std::vector<Vector3R> getVertices() const;
 
-	inline void getMinMaxVertices(Vector3R* out_minVertex, Vector3R* out_maxVertex) const
-	{
-		out_minVertex->set(m_minVertex);
-		out_maxVertex->set(m_maxVertex);
-	}
-
-	inline const Vector3R& getMinVertex() const
-	{
-		return m_minVertex;
-	}
-
-	inline const Vector3R& getMaxVertex() const
-	{
-		return m_maxVertex;
-	}
-
-	inline Vector3R calcCentroid() const
-	{
-		return m_minVertex.add(m_maxVertex).mulLocal(0.5_r);
-	}
-
-	inline Vector3R calcExtents() const
-	{
-		return m_maxVertex.sub(m_minVertex);
-	}
-
-	inline real calcSurfaceArea() const
-	{
-		const Vector3R& extents = calcExtents();
-		return 2.0_r * (extents.x * extents.y + extents.y * extents.z + extents.z * extents.x);
-	}
-
-	inline void setMinVertex(const Vector3R& minVertex)
-	{
-		m_minVertex = minVertex;
-	}
-
-	inline void setMaxVertex(const Vector3R& maxVertex)
-	{
-		m_maxVertex = maxVertex;
-	}
-
-	inline void expand(const Vector3R& amount)
-	{
-		m_minVertex.subLocal(amount);
-		m_maxVertex.addLocal(amount);
-	}
+	void getMinMaxVertices(Vector3R* out_minVertex, Vector3R* out_maxVertex) const;
+	const Vector3R& getMinVertex() const;
+	const Vector3R& getMaxVertex() const;
+	Vector3R calcCentroid() const;
+	Vector3R calcExtents() const;
+	real calcSurfaceArea() const;
+	real calcVolume() const;
+	void setMinVertex(const Vector3R& minVertex);
+	void setMaxVertex(const Vector3R& maxVertex);
+	void expand(const Vector3R& amount);
 
 	std::string toString() const;
 
@@ -84,5 +48,73 @@ private:
 	Vector3R m_minVertex;
 	Vector3R m_maxVertex;
 };
+
+// In-header Implementations:
+
+inline void AABB3D::getMinMaxVertices(Vector3R* const out_minVertex, Vector3R* const out_maxVertex) const
+{
+	PH_ASSERT(out_minVertex && out_maxVertex);
+
+	out_minVertex->set(m_minVertex);
+	out_maxVertex->set(m_maxVertex);
+}
+
+inline const Vector3R& AABB3D::getMinVertex() const
+{
+	return m_minVertex;
+}
+
+inline const Vector3R& AABB3D::getMaxVertex() const
+{
+	return m_maxVertex;
+}
+
+inline Vector3R AABB3D::calcCentroid() const
+{
+	return m_minVertex.add(m_maxVertex).mulLocal(0.5_r);
+}
+
+inline Vector3R AABB3D::calcExtents() const
+{
+	return m_maxVertex.sub(m_minVertex);
+}
+
+inline real AABB3D::calcSurfaceArea() const
+{
+	const Vector3R& extents = calcExtents();
+	return 2.0_r * (extents.x * extents.y + extents.y * extents.z + extents.z * extents.x);
+}
+
+inline real AABB3D::calcVolume() const
+{
+	const Vector3R& extents = calcExtents();
+	return extents.x * extents.y * extents.z;
+}
+
+inline void AABB3D::setMinVertex(const Vector3R& minVertex)
+{
+	m_minVertex = minVertex;
+}
+
+inline void AABB3D::setMaxVertex(const Vector3R& maxVertex)
+{
+	m_maxVertex = maxVertex;
+}
+
+inline void AABB3D::expand(const Vector3R& amount)
+{
+	m_minVertex.subLocal(amount);
+	m_maxVertex.addLocal(amount);
+}
+
+inline bool AABB3D::isPoint() const
+{
+	return m_minVertex.equals(m_maxVertex);
+}
+
+inline bool AABB3D::isFiniteVolume() const
+{
+	return std::isnormal(calcVolume());
+}
 
 }// end namespace ph
