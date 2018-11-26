66d6680553d39669e6b86ff94ecf53c64d13eb42
T.C. Chang <b01502051@ntu.edu.tw>
2018-11-27 02:06:05 +0800

    exported more SDL interfaces

--- Engine/Source/Actor/Material/AbradedOpaque.h
+++ Engine/Source/Actor/Material/AbradedOpaque.h
@@ -35,3 +35,56 @@ public:
 };
 
 }// end namespace ph
+
+/*
+	<SDL_interface>
+
+	<category>  material          </category>
+	<type_name> abraded-opaque    </type_name>
+	<extend>    material.material </extend>
+
+	<name> Abraded Opaque </name>
+	<description>
+		Able to model surfaces ranging from nearly specular to extremely rough
+		appearances.
+	</description>
+
+	<command type="creator">
+		<input name="type" type="string">
+			<description>
+				Possible value are "iso-metallic-ggx" and "aniso-metallic-ggx",
+				for isotropic and anisotropic surface appearances, respectively.
+			</description>
+		</input>
+		<input name="roughness" type="real">
+			<description>
+				Isotropic surface roughness in [0, 1], the material will appear
+				to be smoother with smaller roughness value.
+			</description>
+		</input>
+		<input name="f0" type="vector3">
+			<description>
+				Surface reflectance on normal incidence. This value is expected
+				to be given in linear-SRGB space. When this parameter is used,
+				the underlying Fresnel model will be an approximated one which
+				is pretty popular in real-time graphics.
+			</description>
+		</input>
+		<input name="roughness-u" type="real">
+			<description>
+				Similar to the roughness parameter, but is used for anisotropic 
+				surface appearances. This value controls the U component of 
+				surface roughness.
+			</description>
+		</input>
+		<input name="roughness-v" type="real">
+			<description>
+				Similar to the roughness parameter, but is used for anisotropic
+				surface appearances. This value controls the V component of
+				surface roughness.
+			</description>
+		</input>
+	</command>
+
+	</SDL_interface>
+*/
--- Engine/Source/Actor/Material/AbradedTranslucent.h
+++ Engine/Source/Actor/Material/AbradedTranslucent.h
@@ -32,3 +32,44 @@ public:
 };
 
 }// end namespace ph
+
+/*
+	<SDL_interface>
+
+	<category>  material            </category>
+	<type_name> abraded-translucent </type_name>
+	<extend>    material.material   </extend>
+
+	<name> Abraded Translucent </name>
+	<description>
+		Able to model translucent surfaces with variable roughnesses. Such as
+		frosted glass.
+	</description>
+
+	<command type="creator">
+		<input name="fresnel-type" type="string">
+			<description>
+				Controls the Fresnel model used. Possible values are "schlick-approx"
+				and "exact".
+			</description>
+		</input>
+		<input name="roughness" type="real">
+			<description>
+				Isotropic surface roughness in [0, 1], the material will appear
+				to be smoother with smaller roughness value.
+			</description>
+		</input>
+		<input name="ior-outer" type="real">
+			<description>
+				The index of refraction outside of this material.
+			</description>
+		</input>
+		<input name="ior-inner" type="real">
+			<description>
+				The index of refraction inside of this material.
+			</description>
+		</input>
+	</command>
+
+	</SDL_interface>
+*/
