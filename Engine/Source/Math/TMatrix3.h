a378b3de320386d833fdd8e131820e58f5052e0b
T.C. Chang <b01502051@ntu.edu.tw>
2018-12-24 16:26:43 +0800

    [refactor] made TMatrix3 trivially copyable

--- Engine/Source/Math/TMatrix3.h
+++ Engine/Source/Math/TMatrix3.h
@@ -12,29 +12,32 @@ template<typename T>
 class TMatrix3 final
 {
 public:
-	typedef std::array<std::array<T, 3>, 3> Elements;
+	static TMatrix3 makeIdentity();
+
+public:
+	using Elements = std::array<std::array<T, 3>, 3>;
 
 	Elements m;
 
-	inline TMatrix3();
-	explicit inline TMatrix3(T value);
-	explicit inline TMatrix3(const Elements& elements);
-	inline TMatrix3(const TMatrix3& other);
+	inline TMatrix3() = default;
+	explicit TMatrix3(T value);
+	explicit TMatrix3(const Elements& elements);
+	inline TMatrix3(const TMatrix3& other) = default;
 
 	template<typename U>
-	explicit inline TMatrix3(const TMatrix3<U>& other);
+	explicit TMatrix3(const TMatrix3<U>& other);
 
-	inline TMatrix3& initIdentity();
+	TMatrix3& initIdentity();
 
-	inline TMatrix3 mul(T value) const;
-	inline TMatrix3& mulLocal(T value);
-	inline TMatrix3 mul(const TMatrix3& rhs) const;
-	inline void mul(const TMatrix3& rhs, TMatrix3* out_result) const;
+	TMatrix3 mul(T value) const;
+	TMatrix3& mulLocal(T value);
+	TMatrix3 mul(const TMatrix3& rhs) const;
+	void mul(const TMatrix3& rhs, TMatrix3* out_result) const;
 
-	inline TMatrix3 inverse() const;
-	inline T determinant() const;
+	TMatrix3 inverse() const;
+	T determinant() const;
 
-	inline std::string toString() const;
+	std::string toString() const;
 };
 
 }// end namespace ph
