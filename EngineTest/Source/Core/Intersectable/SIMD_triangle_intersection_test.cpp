69155dd7604f47a467b9ae50e6056b5542731ab6
MarvinChung <marvin852316497@gmail.com>
2019-03-16 23:48:09 +0800

    push miss variables

--- EngineTest/Source/Core/Intersectable/SIMD_triangle_intersection_test.cpp
+++ EngineTest/Source/Core/Intersectable/SIMD_triangle_intersection_test.cpp
@@ -4,6 +4,20 @@
 
 #include <gtest/gtest.h>
 
+#include <simdpp/dispatch/get_arch_gcc_builtin_cpu_supports.h>
+#include <simdpp/dispatch/get_arch_raw_cpuid.h>
+#include <simdpp/dispatch/get_arch_linux_cpuinfo.h>
+
+#if SIMDPP_HAS_GET_ARCH_RAW_CPUID
+#define SIMDPP_USER_ARCH_INFO ::simdpp::get_arch_raw_cpuid()
+#elif SIMDPP_HAS_GET_ARCH_GCC_BUILTIN_CPU_SUPPORTS
+#define SIMDPP_USER_ARCH_INFO ::simdpp::get_arch_gcc_builtin_cpu_supports()
+#elif SIMDPP_HAS_GET_ARCH_LINUX_CPUINFO
+#define SIMDPP_USER_ARCH_INFO ::simdpp::get_arch_linux_cpuinfo()
+#else
+#error "Unsupported platform"
+#endif
+
 TEST(RayWithPackedTriangleTest, HitReturnIsCorrect)
 {
 	using namespace ph;
@@ -84,7 +98,10 @@ TEST(RayWithPackedTriangleTest, HitReturnIsCorrect)
 
 	unsigned seed = (unsigned)time(NULL);
 	srand(seed);
-	int X = 1000000;
+	const int X = 1000000;
+
+	testTriangle normaltri[X];
+	PackedTriangle packedtri[X];
 
 	for(int i = 0; i < 1000000; i ++)
 	{
