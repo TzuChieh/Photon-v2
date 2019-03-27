36ffa76d490b15d7bf90e5b7fb96a9eafa7debeb
T.C. Chang <b01502051@ntu.edu.tw>
2019-03-27 16:16:14 +0800

    better handling of test resource path

--- EngineTest/Source/FileIO/exr_file_io.cpp
+++ EngineTest/Source/FileIO/exr_file_io.cpp
@@ -1,3 +1,5 @@
+#include "config.h"
+
 #include <FileIO/ExrFileReader.h>
 
 #include <gtest/gtest.h>
@@ -6,7 +8,7 @@ using namespace ph;
 
 TEST(ExrFileIOTest, ReadFileInfo)
 {
-	ph::ExrFileReader reader(ph::Path("./2x1_black_white.exr"));
+	ph::ExrFileReader reader(ph::Path(PH_TEST_RESOURCE_PATH("EXR/2x1_black_white.exr")));
 	HdrRgbFrame frame;
 	reader.load(&frame);
 }
---
+++ EngineTest/Source/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define PH_TEST_RESOURCE_ROOT_DIR "./Resource/Test/"
+
+#define PH_TEST_RESOURCE_PATH(relativePath) PH_TEST_RESOURCE_ROOT_DIR relativePath
