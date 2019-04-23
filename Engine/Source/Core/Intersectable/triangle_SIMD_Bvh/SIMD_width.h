17e3c45a9dbabf67c77553b7f5d940df98ac5659
MarvinChung <marvin852316497@gmail.com>
2019-04-23 20:16:31 +0800

    change arch

--- Engine/Source/Core/Intersectable/triangle_SIMD_Bvh/SIMD_width.h
+++ Engine/Source/Core/Intersectable/triangle_SIMD_Bvh/SIMD_width.h
@@ -21,29 +21,16 @@
 // #define SIMDPP_ARCH_POWER_VSX_207
 // #define SIMDPP_ARCH_MIPS_MSA
 
-#define SIMDPP_ARCH_X86_SSE2
+#define SIMDPP_ARCH_X86_SSE3
 #define SIMDPP_EMIT_DISPATCHER 1
-#define SIMDPP_DISPATCH_ARCH1 SIMDPP_ARCH_X86_SSE2 
-#define SIMDPP_DISPATCH_ARCH2 SIMDPP_ARCH_X86_SSE3 
-#define SIMDPP_DISPATCH_ARCH3 SIMDPP_ARCH_X86_SSE4_1 
-#define SIMDPP_DISPATCH_ARCH4 SIMDPP_ARCH_X86_AVX
-#define SIMDPP_DISPATCH_ARCH5 SIMDPP_ARCH_X86_AVX2
+//#define SIMDPP_DISPATCH_ARCH1 SIMDPP_ARCH_X86_SSE2 
+#define SIMDPP_DISPATCH_ARCH1 SIMDPP_ARCH_X86_SSE3 
+#define SIMDPP_DISPATCH_ARCH2 SIMDPP_ARCH_X86_SSE4_1 
+#define SIMDPP_DISPATCH_ARCH3 SIMDPP_ARCH_X86_AVX
+#define SIMDPP_DISPATCH_ARCH4 SIMDPP_ARCH_X86_AVX2
 
 
-#include "simdpp/simd.h"
-#include <simdpp/dispatch/get_arch_gcc_builtin_cpu_supports.h>
-#include <simdpp/dispatch/get_arch_raw_cpuid.h>
-#include <simdpp/dispatch/get_arch_linux_cpuinfo.h>
 
-#if SIMDPP_HAS_GET_ARCH_RAW_CPUID
-#define SIMDPP_USER_ARCH_INFO ::simdpp::get_arch_raw_cpuid()
-#elif SIMDPP_HAS_GET_ARCH_GCC_BUILTIN_CPU_SUPPORTS
-#define SIMDPP_USER_ARCH_INFO ::simdpp::get_arch_gcc_builtin_cpu_supports()
-#elif SIMDPP_HAS_GET_ARCH_LINUX_CPUINFO
-#define SIMDPP_USER_ARCH_INFO ::simdpp::get_arch_linux_cpuinfo()
-#else
-#error "Unsupported platform"
-#endif
 
 
 #include <limits>
