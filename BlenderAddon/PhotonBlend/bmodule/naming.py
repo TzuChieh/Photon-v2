7c45948308b06267c63a1e3ce85a75bbfe6f0ec7
Tzu-Chieh Chang <b01502051@ntu.edu.tw>
2019-08-28 01:43:09 +0800

    [wip] added new naming utilities for SDL resources

--- BlenderAddon/PhotonBlend/bmodule/naming.py
+++ BlenderAddon/PhotonBlend/bmodule/naming.py
@@ -1,7 +1,17 @@
+"""
+Utilities for providing unique names (e.g., through name mangling or other means) for Photon resources in Blender.
+Uniqueness of a name is guaranteed across all supported resource base categories. For example, two object resources will
+never have the same mangled names, and a mesh resource will never have the same mangled name as a material resource.
+
+In situations where name conflict is possible, they are expected to be resolved via the specified decorations.
+"""
+
 import bpy
 
+# TODO: materials from other blend files (library blends) can have the same name, need to somehow distinguish them
 
-def get_decorated_name(name, **decorations):
+# Decorates a name given predefined decoration elements.
+def _get_decorated_name(name, **decorations):
 	decorated_name = name
 	if 'prefix' in decorations:
 		decorated_name = decorations.get('prefix') + "$" + decorated_name
@@ -11,15 +21,40 @@ def get_decorated_name(name, **decorations):
 	return decorated_name
 
 
+# Get a unique resource name for the mesh resource.
 def get_mangled_mesh_name(b_mesh: bpy.types.Mesh, **decorations):
-	return "G" + get_decorated_name(b_mesh.name, **decorations)
+	return "GE" + _get_decorated_name(b_mesh.name, **decorations)
 
 
+# Get a unique resource name for the material resource.
 def get_mangled_material_name(b_material: bpy.types.Material, **decorations):
-	return "M" + get_decorated_name(b_material.name, **decorations)
+	return "MA" + _get_decorated_name(b_material.name, **decorations)
 
 
+# Get a unique resource name for the object resource.
 def get_mangled_object_name(b_object: bpy.types.Object, **decorations):
-	return "O" + get_decorated_name(b_object.name, **decorations)
+	return "OB" + _get_decorated_name(b_object.name, **decorations)
 
-def get_mangled_emissive_image_name(b_material: bpy.types.Material, **decorations):
+
+# Get a unique resource name for the node resource.
+def get_mangled_node_name(b_node: bpy.types.Node, b_material: bpy.types.Material, **decorations):
+	# Material name is required since node name is unique within the same node tree only.
+	joint_name = _get_decorated_name(b_node.name, prefix=b_material.name)
+
+	return "NO" + _get_decorated_name(joint_name, **decorations)
+
+
+def _get_mangled_node_socket_name(b_node_socket: bpy.types.Node, b_material: bpy.types.Material, **decorations):
+	b_owning_node = b_node_socket.node
+	joint_name = _get_decorated_name(b_node_socket.identifier, prefix=b_owning_node.name)
+	joint_name = _get_decorated_name(joint_name, prefix=b_material.name)
+
+	return _get_decorated_name(joint_name, **decorations)
+
+
+def get_mangled_input_node_socket_name(b_node_socket: bpy.types.Node, b_material: bpy.types.Material, **decorations):
+	return "IN" + _get_mangled_node_socket_name(b_node_socket, b_material, **decorations)
+
+
+def get_mangled_output_node_socket_name(b_node_socket: bpy.types.Node, b_material: bpy.types.Material, **decorations):
+	return "OU" + _get_mangled_node_socket_name(b_node_socket, b_material, **decorations)
---
+++ PhotonStudio/Source/plugin/pbrt/PbrtToPhotonPlugin.java
@@ -0,0 +1,41 @@
+package plugin.pbrt;
+
+import plugin.Plugin;
+
+public class PbrtToPhotonPlugin extends Plugin
+{
+	@Override
+	public boolean init()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void run()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean exit()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
