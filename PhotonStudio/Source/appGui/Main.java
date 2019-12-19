c7322fe135898edf48d0d70773c87fc3ac7a0e40
Tzu-Chieh Chang <r05922176@ntu.edu.tw>
2019-12-19 15:42:29 -0400

    made another main method for launching JavaFX app

---
+++ PhotonStudio/Source/appGui/Main.java
@@ -0,0 +1,11 @@
+package appGui;
+
+public class Main
+{
+	// HACK: to start JavaFX app as an ordinary Java application
+	// Reference: https://stackoverflow.com/questions/54891263/import-javafx-11-maven-project-to-eclipse#comment96593966_54901893
+	public static void main(String[] args)
+	{
+		AppMain.main(args);
+	}
+}
