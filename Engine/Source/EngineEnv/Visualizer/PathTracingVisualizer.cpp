ab90c1f0fb75969becc05ff5f7b5ef5b0bf8236e
TzuChieh <r05922176@ntu.edu.tw>
2021-07-23 20:47:38 +0800

    [wip] on adding path tracer

---
+++ Engine/Source/EngineEnv/Visualizer/PathTracingVisualizer.cpp
@@ -0,0 +1,13 @@
+#include "EngineEnv/Visualizer/PathTracingVisualizer.h"
+#include "EngineEnv/CoreCookingContext.h"
+#include "EngineEnv/CoreCookedUnit.h"
+
+namespace ph
+{
+
+void PathTracingVisualizer::cook(const CoreCookingContext& ctx, CoreCookedUnit& cooked)
+{
+	// TODO
+}
+
+}// end namespace ph
--- Engine/Source/EngineEnv/Visualizer/PathTracingVisualizer.h
+++ Engine/Source/EngineEnv/Visualizer/PathTracingVisualizer.h
@@ -14,10 +14,15 @@ class PathTracingVisualizer : public FrameVisualizer
 public:
 	inline PathTracingVisualizer() = default;
 
-	virtual void cook(const CoreCookingContext& ctx, CoreCookedUnit& cooked) = 0;
+	void cook(const CoreCookingContext& ctx, CoreCookedUnit& cooked) override;
+
+	math::TAABB2D<int64> getCropWindowPx() const;
 
 private:
-	math::TAABB2D<int64> m_cropWindowPx;
+	int64 m_cropWindowXPx;
+	int64 m_cropWindowYPx;
+	int64 m_cropWindowWPx;
+	int64 m_cropWindowHPx;
 
 public:
 	PH_DEFINE_SDL_CLASS(TOwnerSdlClass<PathTracingVisualizer>)
@@ -26,12 +31,41 @@ public:
 		clazz.description("Render frames with common path tracing methods.");
 		clazz.baseOn<FrameVisualizer>();
 		
-		TSdlVector2<OwnerType, uint32> frameSizePx("frame-size", &OwnerType::m_frameSizePx);
-		frameSizePx.description("Width and height of the frame in pixels.");
-		frameSizePx.defaultTo();
+		TSdlInt64<OwnerType> cropWindowXPx("rect-x", &OwnerType::m_cropWindowXPx);
+		cropWindowXPx.description("X coordinate of the lower-left corner of the film cropping window.");
+		cropWindowXPx.defaultTo(0);
+		cropWindowXPx.optional();
+		clazz.addField(cropWindowXPx);
+
+		TSdlInt64<OwnerType> cropWindowYPx("rect-y", &OwnerType::m_cropWindowYPx);
+		cropWindowYPx.description("Y coordinate of the lower-left corner of the film cropping window.");
+		cropWindowYPx.defaultTo(0);
+		cropWindowYPx.optional();
+		clazz.addField(cropWindowYPx);
+
+		TSdlInt64<OwnerType> cropWindowWPx("rect-w", &OwnerType::m_cropWindowWPx);
+		cropWindowWPx.description("Width of the film cropping window.");
+		cropWindowWPx.defaultTo(0);
+		cropWindowWPx.optional();
+		clazz.addField(cropWindowWPx);
+
+		TSdlInt64<OwnerType> cropWindowHPx("rect-h", &OwnerType::m_cropWindowHPx);
+		cropWindowHPx.description("Height of the film cropping window.");
+		cropWindowHPx.defaultTo(0);
+		cropWindowHPx.optional();
+		clazz.addField(cropWindowHPx);
 
 		return clazz;
 	}
 };
+
+// In-header Implementations:
+
+inline math::TAABB2D<int64> PathTracingVisualizer::getCropWindowPx() const
+{
+	return math::TAABB2D<int64>(
+		{m_cropWindowXPx, m_cropWindowYPx}, 
+		{m_cropWindowXPx + m_cropWindowWPx, m_cropWindowYPx + m_cropWindowHPx});
+}
 
 }// end namespace ph
