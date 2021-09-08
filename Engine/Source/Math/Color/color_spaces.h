662c9a77d08bcf213d6803b8de714e5341678a79
TzuChieh <r05922176@ntu.edu.tw>
2021-09-09 00:43:17 +0800

    [wip] new color space funcs

--- Engine/Source/Math/Color/color_spaces.h
+++ Engine/Source/Math/Color/color_spaces.h
@@ -15,15 +15,51 @@ template<typename T>
 concept CHasColorSpaceProperties = requires ()
 {
 	{ T::isTristimulus() } noexcept -> std::same_as<bool>;
-	{ T::isSpectral() } noexcept -> std::same_as<bool>;
 	{ T::getReferenceWhite() } noexcept -> std::same_as<EReferenceWhite>;
 };
 
 template<typename T>
-concept CSupportsCIEXYZConversions = requires (const Vector3R& CIEXYZColor)
+concept CSupportsCIEXYZConversions = requires (const Vector3R& thisColor, const Vector3R& CIEXYZColor)
 {
-	{ T::toCIEXYZ() } noexcept -> std::same_as<Vector3R>;
+	{ T::toCIEXYZ(thisColor) } noexcept -> std::same_as<Vector3R>;
 	{ T::fromCIEXYZ(CIEXYZColor) } noexcept -> std::same_as<Vector3R>;
+};
+
+template<typename T>
+concept CColorSpaceDefinition = CHasColorSpaceProperties<T>;
+
+template<EColorSpace COLOR_SPACE>
+class TColorSpaceDefinition final
+{
+	static_assert(COLOR_SPACE == EColorSpace::UNSPECIFIED,
+		"No definition for the specified COLOR_SPACE.");
+};
+
+template<>
+class TColorSpaceDefinition<EColorSpace::CIE_XYZ> final
+{
+public:
+	inline static bool isTristimulus()
+	{
+		return true;
+	}
+
+	inline static EReferenceWhite getReferenceWhite()
+	{
+		return EReferenceWhite::D65;
+	}
+
+	inline static Vector3R toCIEXYZ(const Vector3R& thisColor)
+	{
+		// Already in CIE-XYZ space, simply return the source color.
+		return thisColor;
+	}
+
+	inline static Vector3R fromCIEXYZ(const Vector3R& CIEXYZColor)
+	{
+		// Already in CIE-XYZ space, simply return the source color.
+		return CIEXYZColor;
+	}
 };
 
 }// end namespace ph::math
