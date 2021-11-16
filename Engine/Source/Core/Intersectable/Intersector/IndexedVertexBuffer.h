e8f8fe3f7a894eed055d035c9e0866e53e6b6793
TzuChieh <r05922176@ntu.edu.tw>
2021-11-17 01:00:23 +0800

    [wip] on adding vertex buffer

---
+++ Engine/Source/Core/Intersectable/Intersector/IndexedVertexBuffer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace ph
+{
+
+enum class EVertexEntry
+{
+	Position_0
+};
+
+enum class EVertexElement
+{
+	VE_Float16,
+	VE_Float32,
+	VE_Fixed16,
+	VE_Fixed32,
+	VE_
+};
+
+class IndexedVertexBuffer final
+{
+public:
+
+private:
+};
+
+}// end namespace ph
