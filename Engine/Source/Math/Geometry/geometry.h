9ad60eb3cd81dbbd7b0259390dd33836a03e2c4b
TzuChieh <r05922176@ntu.edu.tw>
2021-11-20 01:46:25 +0800

    add octahedron normal encoding

--- Engine/Source/Math/Geometry/geometry.h
+++ Engine/Source/Math/Geometry/geometry.h
@@ -50,12 +50,47 @@ inline TVector2<T> octahedron_diamond_mirror(const TVector2<T>& point)
 // TODO: precise version of the encoding (see [2])
 
 /*! @brief Encode a unit vector using octahedron projection.
-@return Encoded unit vector in [-1, 1] for any bit size. 
+@param unitVec The unit vector to be encoded. Assumed being normalized already.
+@return Encoded unit vector in [0, 1] for any bit size. 
 */
-//template<typename T>
-//inline TVector2<T> octahedron_unit_vector_encode(const TVector3<T>& vec)
-//{
-//	const TVector3<T> octahedronProj = vec.abs().
-//}
+template<typename T>
+inline TVector2<T> octahedron_unit_vector_encode(const TVector3<T>& unitVec)
+{
+	TVector3<T> octahedronProj = unitVec / unitVec.abs().sum();
+	if(octahedronProj.z() < 0)
+	{
+		const TVector2<T> mirroredXYProj = detail::octahedron_diamond_mirror({octahedronProj.x(), octahedronProj.y()});
+		octahedronProj.x() = mirroredXYProj.x();
+		octahedronProj.y() = mirroredXYProj.y();
+	}
+
+	return TVector2<T>(octahedronProj.x(), octahedronProj.y()) * static_cast<T>(0.5) + static_cast<T>(0.5);
+}
+
+/*! @brief Decode and get a unit vector using octahedron projection.
+@param encodedVal The encoded unit vector (in [0, 1] for any bit size) to be decoded. 
+@return Decoded unit vector (normalized).
+*/
+template<typename T>
+inline TVector3<T> octahedron_unit_vector_decode(const TVector2<T>& encodedVal)
+{
+	// Note that https://knarkowicz.wordpress.com/2014/04/16/octahedron-normal-vector-encoding/
+	// provides an alternative approach that is faster on GPU.
+
+	const TVector2<T> expandedEncodedVal = encodedVal * static_cast<T>(2) - static_cast<T>(1);
+
+	TVector3<T> octahedronProj(
+		expandedEncodedVal.x(),
+		expandedEncodedVal.y(),
+		static_cast<T>(1) - expandedEncodedVal.abs().sum());
+	if(octahedronProj.z() < 0)
+	{
+		const TVector2<T> mirroredXYProj = detail::octahedron_diamond_mirror({octahedronProj.x(), octahedronProj.y()});
+		octahedronProj.x() = mirroredXYProj.x();
+		octahedronProj.y() = mirroredXYProj.y();
+	}
+
+	return octahedronProj.normalize();
+}
 
 }// end namespace ph::math
