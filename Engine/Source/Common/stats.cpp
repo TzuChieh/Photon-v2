1b0361c35336872b6bce9bde2d8244d82b385267
TzuChieh <r05922176@ntu.edu.tw>
2021-12-23 01:08:50 +0800

    add basic scoped timer macros

--- Engine/Source/Common/stats.cpp
+++ Engine/Source/Common/stats.cpp
@@ -1,4 +1,10 @@
 #include "Common/stats.h"
+#include "Common/assertion.h"
+
+#include <utility>
+#include <mutex>
+#include <format>
+#include <unordered_map>
 
 namespace ph
 {
@@ -6,12 +12,191 @@ namespace ph
 namespace detail::stats
 {
 
-// TODO: init to 0 and register
-TimeCounter::TimeCounter()
+namespace
 {
 
+inline std::mutex& TIME_STATS_MUTEX()
+{
+	static std::mutex locker;
+	return locker;
+}
+
+inline std::vector<TimeCounter*>& TIME_COUNTERS()
+{
+	static std::vector<TimeCounter*> counters;
+	return counters;
+}
+
+}// end anonymous namespace
+
+TimeCounter::TimeCounter(std::string name, std::string category) :
+	name(std::move(name)), category(std::move(category)), totalMicroseconds(0), count(0)
+{
+	std::lock_guard<std::mutex> lock(TIME_STATS_MUTEX());
+
+	TIME_COUNTERS().push_back(this);
+}
+
+void TimeCounter::addMicroseconds(const std::uint64_t microseconds)
+{
+	count.fetch_add(1, std::memory_order_relaxed);
+	totalMicroseconds.fetch_add(microseconds, std::memory_order_relaxed);
+}
+
+ScopedTimer::ScopedTimer(TimeCounter& counter) :
+	counter(counter), startTime(Clock::now())
+{}
+
+ScopedTimer::~ScopedTimer()
+{
+	using TimeUnit = std::chrono::microseconds;
+
+	const auto& duration = Clock::now() - startTime;
+	const auto& delta    = std::chrono::duration_cast<TimeUnit>(duration).count();
+	counter.addMicroseconds(static_cast<std::uint64_t>(delta));
 }
 
 }// end namespace detail::stats
+
+TimerStatsReport::TimeRecord::TimeRecord() :
+	name(), category(), totalMicroseconds(0), count(0)
+{}
+
+TimerStatsReport::GroupedTimeRecord::GroupedTimeRecord() :
+	groupName(), totalMicroseconds(0), count(0), subgroups()
+{}
+
+TimerStatsReport::TimerStatsReport() :
+	m_records()
+{
+	std::lock_guard<std::mutex> lock(detail::stats::TIME_STATS_MUTEX());
+
+	m_records.reserve(detail::stats::TIME_COUNTERS().size());
+	for(const auto& timeCounter : detail::stats::TIME_COUNTERS())
+	{
+		TimeRecord record;
+		record.name              = timeCounter->name;
+		record.category          = timeCounter->category;
+		record.totalMicroseconds = timeCounter->totalMicroseconds.load(std::memory_order_relaxed);
+		record.count             = timeCounter->count.load(std::memory_order_relaxed);
+
+		m_records.push_back(record);
+	}
+}
+
+TimerStatsReport::GroupedTimeRecord TimerStatsReport::getGroupedTimeRecord() const
+{
+	return makeGroupedTimeRecordRecursive(m_records);
+}
+
+std::string TimerStatsReport::proportionalReport() const
+{
+	std::string reportStr;
+	reportStr.reserve(128 * m_records.size());
+
+	std::uint64_t totalUSOfAllRecords = 0;
+	std::unordered_map<std::string, std::uint64_t> categoryToTotalUS;
+	for(const TimeRecord& record : m_records)
+	{
+		totalUSOfAllRecords += record.totalMicroseconds;
+
+		// Scalar types are zero initialized by the map
+		categoryToTotalUS[record.category] += record.totalMicroseconds;
+	}
+
+	for(const auto& [category, categoryTotalUS] : categoryToTotalUS)
+	{
+		const auto categoryPercentage = 
+			static_cast<double>(categoryTotalUS) /
+			static_cast<double>(totalUSOfAllRecords) *
+			100.0;
+
+		reportStr += std::format(
+			"- [{:8.4f}%] -> {}:\n",
+			categoryPercentage, category);
+
+		for(const TimeRecord& record : m_records)
+		{
+			const auto recordPercentage = 
+				static_cast<double>(record.totalMicroseconds) /
+				static_cast<double>(categoryTotalUS) *
+				100.0;
+
+			reportStr += std::format(
+				"----- {} [{:8.4f}%] -> {}\n",
+				recordPercentage, record.name);
+		}
+	}
+
+	return reportStr;
+}
+
+std::string TimerStatsReport::rawReport() const
+{
+	std::string reportStr;
+	reportStr.reserve(128 * m_records.size());
+	for(const TimeRecord& record : m_records)
+	{
+		reportStr += std::format(
+			"category: {}, name: {}, total microseconds: {}, counts: {}\n",
+			record.category, record.name, record.totalMicroseconds, record.count);
+	}
+
+	return reportStr;
+}
+
+TimerStatsReport::GroupedTimeRecord TimerStatsReport::makeGroupedTimeRecordRecursive(
+	const std::vector<TimeRecord>& records)
+{
+	GroupedTimeRecord groupedRecord;
+
+	std::unordered_map<std::string, std::vector<TimeRecord>> subgroupNameToRecords;
+	for(const TimeRecord& record : records)
+	{
+		groupedRecord.totalMicroseconds += record.totalMicroseconds;
+		groupedRecord.count             += record.count;
+
+		if(record.category.empty())
+		{
+			GroupedTimeRecord leafGroup;
+			leafGroup.groupName         = record.name;
+			leafGroup.totalMicroseconds = record.totalMicroseconds;
+			leafGroup.count             = record.count;
+
+			groupedRecord.subgroups.push_back(leafGroup);
+		}
+		else
+		{
+			const auto underscorePos = record.category.find('_');
+			const auto subgroupName = underscorePos != std::string::npos
+				? record.category.substr(0, underscorePos)
+				: record.category;
+
+			TimeRecord subgroupRecord = record;
+			subgroupRecord.category = underscorePos != std::string::npos
+				? record.category.substr(underscorePos + 1)
+				: "";
+
+			subgroupNameToRecords[subgroupName].push_back(subgroupRecord);
+		}
+	}
+
+	for(const auto& [subgroupName, subgroupRecords] : subgroupNameToRecords)
+	{
+		GroupedTimeRecord subgroup = makeGroupedTimeRecordRecursive(subgroupRecords);
+		subgroup.groupName = subgroupName;
+
+		groupedRecord.subgroups.push_back(subgroup);
+	}
+
+	return groupedRecord;
+}
+
+std::string TimerStatsReport::makeProportionalReportRecursive(
+	const GroupedTimeRecord& records, const std::string& linePrefix)
+{
+	std::string reportStr;
+	// TODO
+}
 
 }// end namespace ph
--- Engine/Source/Common/stats.h
+++ Engine/Source/Common/stats.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <atomic>
+#include <chrono>
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <vector>
 
 namespace ph
 {
@@ -10,17 +15,85 @@ namespace detail::stats
 
 struct TimeCounter final
 {
-	std::atomic_uint64_t totalMS;
+	const std::string    name;
+	const std::string    category;
+	std::atomic_uint64_t totalMicroseconds;
 	std::atomic_uint64_t count;
 
-	TimeCounter();
+	TimeCounter(std::string name, std::string category);
+	void addMicroseconds(std::uint64_t microseconds);
 };
 
 struct ScopedTimer final
 {
-	// TODO: ref to time counter
+	using Clock = std::chrono::steady_clock;
+
+	TimeCounter&      counter;
+	Clock::time_point startTime;
+
+	explicit ScopedTimer(TimeCounter& counter);
+	~ScopedTimer();
 };
 
 }// end namespace detail::stats
 
+class TimerStatsReport final
+{
+public:
+	struct TimeRecord final
+	{
+		std::string   name;
+		std::string   category;
+		std::uint64_t totalMicroseconds;
+		std::uint64_t count;
+
+		TimeRecord();
+	};
+
+	struct GroupedTimeRecord final
+	{
+		std::string   groupName;
+		std::uint64_t totalMicroseconds;
+		std::uint64_t count;
+
+		std::vector<GroupedTimeRecord> subgroups;
+
+		GroupedTimeRecord();
+	};
+
+	TimerStatsReport();
+
+	GroupedTimeRecord getGroupedTimeRecord() const;
+	std::string proportionalReport() const;
+	std::string rawReport() const;
+
+private:
+	std::vector<TimeRecord> m_records;
+
+	static GroupedTimeRecord makeGroupedTimeRecordRecursive(const std::vector<TimeRecord>& records);
+
+	static std::string makeProportionalReportRecursive(
+		const GroupedTimeRecord& records, const std::string& linePrefix);
+};
+
 }// end namespace ph
+
+#define PH_DEFINE_INLINE_TIMER_STAT(statName, categoryName)\
+	inline ::ph::detail::stats::TimeCounter& internal_impl_time_counter_access_##statName()\
+	{\
+		static ::ph::detail::stats::TimeCounter counter(#statName, #categoryName);\
+		return counter;\
+	}
+
+#define PH_DEFINE_INTERNAL_TIMER_STAT(statName, categoryName)\
+	namespace\
+	{\
+		PH_DEFINE_INLINE_TIMER_STAT(statName, categoryName);\
+	}
+
+#define PH_DEFINE_EXTERNAL_TIMER_STAT(statName, categoryName)\
+	PH_DEFINE_INLINE_TIMER_STAT(statName, categoryName)
+
+#define PH_SCOPED_TIMER(statName)\
+	::ph::detail::stats::ScopedTimer internal_impl_scopedTimer##statName(\
+		internal_impl_time_counter_access_##statName())
