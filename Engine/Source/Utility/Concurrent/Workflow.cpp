4220d3b9192030546ddb53e5082bdcb5ed6d8111
TzuChieh <r05922176@ntu.edu.tw>
2022-01-15 18:47:06 +0800

    basic Workflow (without topo sort)

--- Engine/Source/Utility/Concurrent/Workflow.cpp
+++ Engine/Source/Utility/Concurrent/Workflow.cpp
@@ -2,44 +2,75 @@
 
 #include "Utility/Concurrent/Workflow.h"
 #include "Common/assertion.h"
+#include "Utility/Concurrent/FixedSizeThreadPool.h"
 
 #include <stdexcept>
-#include <format>
-#include <limits>
-#include <utility>
 #include <algorithm>
 
 namespace ph
 {
 
+Workflow::WorkHandle::WorkHandle() :
+	WorkHandle(static_cast<std::size_t>(-1), nullptr)
+{}
+
 Workflow::WorkHandle::WorkHandle(const std::size_t workId, Workflow* const workflow) :
 	m_workId(workId), m_workflow(workflow)
 {}
 
-void Workflow::WorkHandle::runsBefore(WorkHandle succeedingWork)
+void Workflow::WorkHandle::runsBefore(const WorkHandle succeedingWork)
 {
-
+	PH_ASSERT(m_workflow);
+	m_workflow->dependsOn(succeedingWork, *this);
 }
 
-void Workflow::WorkHandle::runsAfter(WorkHandle preceedingWork)
+void Workflow::WorkHandle::runsAfter(const WorkHandle preceedingWork)
 {
-
+	PH_ASSERT(m_workflow);
+	m_workflow->dependsOn(*this, preceedingWork);
 }
 
-void Workflow::WorkHandle::runsBefore(std::initializer_list<WorkHandle> succeedingWorks)
+//Workflow::ControlledWork::ControlledWork(Work work, Workflow* const workflow) :
+//	m_work(std::move(work)), m_workflow(workflow)
+//{
+//	PH_ASSERT(m_work);
+//	PH_ASSERT(m_workflow);
+//}
+//
+//void Workflow::ControlledWork::operator () ()
+//{
+//	m_work();
+//}
+
+Workflow::ManagedWork::ManagedWork(const WorkHandle handle) :
+	m_handle(handle)
 {
-	for(WorkHandle work : succeedingWorks)
+	PH_ASSERT(m_handle);
+}
+
+void Workflow::ManagedWork::operator () ()
+{
+	PH_ASSERT(m_handle.getWorkflow()->m_workDoneFlags);
+
+	const auto  workId            = m_handle.getWorkId();
+	const auto& idToDependencyIds = m_handle.getWorkflow()->m_idToDependencyIds;
+	auto&       workDoneFlags     = m_handle.getWorkflow()->m_workDoneFlags;
+
+	// Wait for all dependencies to finish first
+	const std::vector<std::size_t>& dependencyIds = idToDependencyIds[workId];
+	for(std::size_t dependencyWorkId = 0; dependencyWorkId < dependencyIds.size(); ++dependencyWorkId)
 	{
-		runsBefore(work);
-	}
-}
+		workDoneFlags[dependencyWorkId].wait(false, std::memory_order_acquire);
 
-void Workflow::WorkHandle::runsAfter(std::initializer_list<WorkHandle> preceedingWorks)
-{
-	for(WorkHandle work : preceedingWorks)
-	{
-		runsAfter(work);
+		PH_ASSERT(workDoneFlags[dependencyWorkId].test(std::memory_order_relaxed));
 	}
+
+	// All depending works are done--now we can do this work
+	m_handle.getWorkflow()->m_works[workId]();
+
+	// Signify this work is done so works depending on it can unwait/unblock
+	const bool hasAlreadyDone = workDoneFlags[workId].test_and_set(std::memory_order_release);
+	PH_ASSERT(!hasAlreadyDone);
 }
 
 Workflow::Workflow() :
@@ -47,9 +78,9 @@ Workflow::Workflow() :
 {}
 
 Workflow::Workflow(const std::size_t numExpectedWorks) :
-	m_works(), m_idToDependentIds()
+	m_works(), m_idToDependencyIds(), m_workDoneFlags()
 {
-	m_idToDependentIds.reserve(std::max<std::size_t>(numExpectedWorks, 128));
+	m_idToDependencyIds.reserve(std::max<std::size_t>(numExpectedWorks, 128));
 }
 
 Workflow::WorkHandle Workflow::addWork(Work work)
@@ -60,15 +91,35 @@ Workflow::WorkHandle Workflow::addWork(Work work)
 	}
 
 	m_works.push_back(std::move(work));
-	m_idToDependentIds.push_back(std::vector<std::size_t>());
-	PH_ASSERT_EQ(m_works.size(), m_idToDependentIds.size());
+	m_idToDependencyIds.push_back(std::vector<std::size_t>());
+	PH_ASSERT_EQ(m_works.size(), m_idToDependencyIds.size());
 
 	return WorkHandle(m_works.size() - 1, this);
 }
 
-Workflow::WorkHandle Workflow::addWorks(std::initializer_list<Work> works)
+void Workflow::dependsOn(const WorkHandle target, const WorkHandle targetDependency)
 {
-	// TODO
+	ensureValidWorkHandle(target);
+	ensureValidWorkHandle(targetDependency);
+
+	PH_ASSERT_LT(target.getWorkId(), m_idToDependencyIds.size());
+	m_idToDependencyIds[target.getWorkId()].push_back(targetDependency.getWorkId());
+}
+
+void Workflow::runAndWaitAllWorks(FixedSizeThreadPool& workers)
+{
+	m_workDoneFlags = std::make_unique<std::atomic_flag[]>(numWorks());
+
+	// TODO: topo sort
+	for(std::size_t workId = 0; workId < numWorks(); ++workId)
+	{
+		workers.queueWork(ManagedWork(WorkHandle(workId, this)));
+	}
+
+	workers.waitAllWorks();
+
+	// We do not need the flags anymore, release memory
+	m_workDoneFlags = nullptr;
 }
 
 std::size_t Workflow::numWorks() const
@@ -76,62 +127,12 @@ std::size_t Workflow::numWorks() const
 	return m_works.size();
 }
 
-template<typename Index, bool IS_DIRECTED>
-inline void TAdjacencyListGraph<Index, IS_DIRECTED>::addVertex()
+void Workflow::ensureValidWorkHandle(const WorkHandle work) const
 {
-	addVertex(1);
-}
-
-template<typename Index, bool IS_DIRECTED>
-inline void TAdjacencyListGraph<Index, IS_DIRECTED>::addVertices(const std::size_t numAddedVertices)
-{
-	m_vertexToNeighbors.resize(m_vertexToNeighbors.size() + numAddedVertices);
-}
-
-template<typename Index, bool IS_DIRECTED>
-inline std::size_t TAdjacencyListGraph<Index, IS_DIRECTED>::numVertices() const
-{
-	return m_vertexToNeighbors.size();
-}
-
-template<typename Index, bool IS_DIRECTED>
-inline void TAdjacencyListGraph<Index, IS_DIRECTED>::addEdge(std::size_t fromVertex, std::size_t toVertex)
-{
-	if constexpr(!IS_DIRECTED)
+	if(!work || work.getWorkflow() != this)
 	{
-		// For undirected graph, edge always start with the smaller vertex index, this saves space and time
-		if(fromVertex > toVertex)
-		{
-			std::swap(fromVertex, toVertex);
-		}
-
-		PH_ASSERT_LE(fromVertex, toVertex);
+		throw std::invalid_argument("invalid work handle detected");
 	}
-
-	if(toVertex > std::numeric_limits<Index>::max())
-	{
-		throw std::overflow_error(std::format(
-			"second edge vertex index {} cannot be represented (overflow) by Index (max: {})",
-			toVertex, std::numeric_limits<Index>::max()));
-	}
-
-	PH_ASSERT_LT(fromVertex, m_vertexToNeighbors.size());
-	PH_ASSERT_LT(toVertex, m_vertexToNeighbors.size());
-	m_vertexToNeighbors[fromVertex].push_back(static_cast<Index>(toVertex));
-}
-
-template<typename Index, bool IS_DIRECTED>
-inline std::size_t TAdjacencyListGraph<Index, IS_DIRECTED>::numDegrees(const std::size_t vertex) const
-{
-	PH_ASSERT_LT(vertex, m_vertexToNeighbors.size());
-	return m_vertexToNeighbors[vertex].size();
-}
-
-template<typename Index, bool IS_DIRECTED>
-inline bool TAdjacencyListGraph<Index, IS_DIRECTED>::hasEdge(const std::size_t fromVertex, const std::size_t toVertex) const
-{
-	PH_ASSERT_LT(vertex, m_vertexToNeighbors.size());
-	for()
 }
 
 }// end namespace ph
--- Engine/Source/Utility/Concurrent/Workflow.h
+++ Engine/Source/Utility/Concurrent/Workflow.h
@@ -1,18 +1,27 @@
 #pragma once
 
+#include "Utility/INoCopyAndMove.h"
+
 #include <vector>
 #include <cstddef>
 #include <functional>
-#include <initializer_list>
+#include <array>
+#include <utility>
+#include <memory>
+#include <atomic>
+
+namespace ph { class FixedSizeThreadPool; }
 
 namespace ph
 {
 
-class Workflow final
+class Workflow final : private INoCopyAndMove
 {
 public:
 	using Work = std::function<void()>;
 
+	/*! @brief Lightweight handle for an added work.
+	*/
 	class WorkHandle final
 	{
 		friend class Workflow;
@@ -20,10 +29,20 @@ public:
 	public:
 		void runsBefore(WorkHandle succeedingWork);
 		void runsAfter(WorkHandle preceedingWork);
-		void runsBefore(std::initializer_list<WorkHandle> succeedingWorks);
-		void runsAfter(std::initializer_list<WorkHandle> preceedingWorks);
+
+		template<std::size_t N>
+		void runsBefore(std::array<WorkHandle, N> succeedingWorks);
+
+		template<std::size_t N>
+		void runsAfter(std::array<WorkHandle, N> preceedingWorks);
+
+		std::size_t getWorkId() const;
+		Workflow* getWorkflow() const;
+
+		operator bool () const;
 
 	private:
+		WorkHandle();
 		WorkHandle(std::size_t workId, Workflow* workflow);
 
 		std::size_t m_workId;
@@ -35,12 +54,78 @@ public:
 	explicit Workflow(std::size_t numExpectedWorks);
 
 	WorkHandle addWork(Work work);
-	WorkHandle addWorks(std::initializer_list<Work> works);
+
+	template<std::size_t N>
+	std::array<WorkHandle, N> addWorks(std::array<Work, N> works);
+
+	void dependsOn(WorkHandle target, WorkHandle targetDependency);
+	void runAndWaitAllWorks(FixedSizeThreadPool& workers);
+
 	std::size_t numWorks() const;
 
 private:
+	class ManagedWork final
+	{
+	public:
+		explicit ManagedWork(WorkHandle handle);
+
+		void operator () ();
+
+	private:
+		WorkHandle m_handle;
+	};
+
+	void ensureValidWorkHandle(WorkHandle work) const;
+
 	std::vector<Work>                     m_works;
-	std::vector<std::vector<std::size_t>> m_idToDependentIds;
+	std::vector<std::vector<std::size_t>> m_idToDependencyIds;
+	std::unique_ptr<std::atomic_flag[]>   m_workDoneFlags;
 };
+
+// In-header Implementations:
+
+template<std::size_t N>
+inline std::array<Workflow::WorkHandle, N> Workflow::addWorks(std::array<Work, N> works)
+{
+	std::array<WorkHandle, N> workHandles;
+	for(std::size_t i = 0; i < N; ++i)
+	{
+		workHandles[i] = addWork(std::move(works[i]));
+	}
+	return workHandles;
+}
+
+template<std::size_t N>
+inline void Workflow::WorkHandle::runsBefore(std::array<WorkHandle, N> succeedingWorks)
+{
+	for(WorkHandle work : succeedingWorks)
+	{
+		runsBefore(std::move(work));
+	}
+}
+
+template<std::size_t N>
+inline void Workflow::WorkHandle::runsAfter(std::array<WorkHandle, N> preceedingWorks)
+{
+	for(WorkHandle work : preceedingWorks)
+	{
+		runsAfter(std::move(work));
+	}
+}
+
+inline std::size_t Workflow::WorkHandle::getWorkId() const
+{
+	return m_workId;
+}
+
+inline Workflow* Workflow::WorkHandle::getWorkflow() const
+{
+	return m_workflow;
+}
+
+inline Workflow::WorkHandle::operator bool () const
+{
+	return m_workId != static_cast<std::size_t>(-1) && m_workflow != nullptr;
+}
 
 }// end namespace ph
