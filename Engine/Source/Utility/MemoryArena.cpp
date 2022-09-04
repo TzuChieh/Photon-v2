6946b64e5582fbe16412c80e0f33ae7108b30fbf
T.C. Chang <b01502051@ntu.edu.tw>
2022-09-05 02:17:30 +0800

    implement MemoryArena

--- Engine/Source/Utility/MemoryArena.cpp
+++ Engine/Source/Utility/MemoryArena.cpp
@@ -1,5 +1,9 @@
 #include "Utility/MemoryArena.h"
 #include "Common/config.h"
+#include "Common/assertion.h"
+#include "Common/os.h"
+
+#include <new>
 
 namespace ph
 {
@@ -14,23 +18,96 @@ inline std::size_t determine_block_size(const std::size_t blockSizeInBytes)
 		: PH_MEMORY_ARENA_DEFAULT_BLOCK_SIZE_IN_BYTES;
 }
 
+inline auto allocate_block(const std::size_t blockSizeInBytes)
+	-> TAlignedMemoryUniquePtr<std::byte>
+{
+	constexpr auto MAX_ALIGN_SIZE = alignof(std::max_align_t);
+	const     auto cacheSize      = os::get_L1_cache_line_size_in_bytes();
+
+	PH_ASSERT_MSG(cacheSize >= MAX_ALIGN_SIZE && cacheSize % MAX_ALIGN_SIZE == 0,
+		"Current allocation scheme will result in degraded performance due to cache miss if the above "
+		"condition is not met.");
+
+	TAlignedMemoryUniquePtr<std::byte> memory = make_aligned_memory<std::byte>(blockSizeInBytes, cacheSize);
+	if(!memory)
+	{
+		throw std::bad_alloc();
+	}
+	return memory;
+}
+
 }// end anonymous namespace
 
 MemoryArena::MemoryArena()
-{
-
-}
+	: MemoryArena(determine_block_size(0), 0)
+{}
 
 MemoryArena::MemoryArena(const std::size_t blockSizeInBytes, const std::size_t numDefaultBlocks)
-	: m_blocks()
-	, m_blockSizeInBytes;
-std::size_t m_currentBlockIdx;
-std::byte* m_currentBlockPtr;
-std::size_t m_currentByteIdxInBlock;
-std::size_t m_numUsedBytes;
-
+	: m_blocks               (numDefaultBlocks)
+	, m_blockSizeInBytes     (determine_block_size(blockSizeInBytes))
+	, m_currentBlockIdx      (0)
+	, m_blockPtr             (nullptr)
+	, m_remainingBytesInBlock(0)
+	, m_numUsedBytes         (0)
 {
+	PH_ASSERT_EQ(m_blocks.size(), numDefaultBlocks);
+	for(TAlignedMemoryUniquePtr<std::byte>& block : m_blocks)
+	{
+		block = allocate_block(m_blockSizeInBytes);
+	}
 
+	if(!m_blocks.empty())
+	{
+		m_blockPtr = m_blocks[0].get();
+		m_remainingBytesInBlock = m_blockSizeInBytes;
+	}
+}
+
+std::byte* MemoryArena::allocRaw(const std::size_t numBytes, const std::size_t alignmentInBytes)
+{
+	// It is impossible to allocate memory larger than the fixed block size
+	if(numBytes > m_blockSizeInBytes) [[unlikely]]
+	{
+		throw std::bad_alloc();
+	}
+
+	// NOTE: A better strategy would be keeping a record of remaining space in each block, then pick the
+	// most suitable one to allocate. Here we just use the next block to allocate, trading space for speed. 
+
+	// Use a new block if there is not enough space left
+	if(m_remainingBytesInBlock < numBytes)
+	{
+		// Allocate a new one if there are no blocks left
+		if(m_currentBlockIdx + 1 == m_blocks.size())
+		{
+			m_blocks.push_back(allocate_block(m_blockSizeInBytes));
+		}
+
+		PH_ASSERT_LT(m_currentBlockIdx + 1, m_blocks.size());
+		m_blockPtr = m_blocks[++m_currentBlockIdx].get();
+		m_remainingBytesInBlock = m_blockSizeInBytes;
+	}
+
+	PH_ASSERT(m_blockPtr);
+	PH_ASSERT_GE(m_remainingBytesInBlock, numBytes);
+
+	void* newBlockPtr = m_blockPtr;
+	void* const alignedPtr = std::align(alignmentInBytes, numBytes, newBlockPtr, m_remainingBytesInBlock);
+	
+	// It is possible that the alignment requirement is impossible to met due to insufficient bytes
+	// remaining in the block
+	if(!alignedPtr) [[unlikely]]
+	{
+		throw std::bad_alloc();
+	}
+
+	// `std::align()` only adjusts `newBlockPtr` to the aligned memory location
+	m_blockPtr = static_cast<std::byte*>(newBlockPtr) + numBytes;
+
+	// `std::align()` only decreases `m_remainingBytesInBlock` by the number of bytes used for alignment
+	m_remainingBytesInBlock -= numBytes;
+
+	return static_cast<std::byte*>(alignedPtr);
 }
 
 }// end namespace ph
--- Engine/Source/Utility/MemoryArena.h
+++ Engine/Source/Utility/MemoryArena.h
@@ -6,6 +6,10 @@
 
 #include <cstddef>
 #include <vector>
+#include <span>
+#include <utility>
+#include <memory>
+#include <type_traits>
 
 namespace ph
 {
@@ -13,26 +17,71 @@ namespace ph
 class MemoryArena final : private IMoveOnly
 {
 public:
+	/*! @brief Empty arena without any allocation performed yet.
+	*/
 	MemoryArena();
+
 	MemoryArena(std::size_t blockSizeInBytes, std::size_t numDefaultBlocks);
-	MemoryArena(MemoryArena&& other);
+	inline MemoryArena(MemoryArena&& other) = default;
+
+	std::byte* allocRaw(std::size_t numBytes, std::size_t alignmentInBytes = alignof(std::max_align_t));
 
 	std::size_t numUsedBytes() const;
 	std::size_t numAllocatedBytes() const;
 	std::size_t getBlockSizeInBytes() const;
 	std::size_t numAllocatedBlocks() const;
 
-	MemoryArena& operator = (MemoryArena&& rhs);
+	inline MemoryArena& operator = (MemoryArena&& rhs) = default;
+
+	template<typename T>
+	inline T* alloc()
+		requires std::is_trivially_destructible_v<T>
+	{
+		return reinterpret_cast<T*>(allocRaw(sizeof(T), alignof(T)));
+	}
+
+	template<typename T>
+	inline std::span<T> allocArray(const std::size_t arraySize)
+		requires std::is_trivially_destructible_v<T>
+	{
+		return std::span<T>(allocRaw(sizeof(T) * arraySize, alignof(T)), arraySize);
+	}
+
+	template<typename T, typename... Args>
+	inline T* make(Args&&... args)
+		requires std::is_trivially_destructible_v<T>
+	{
+		return std::construct_at<T, Args>(alloc<T>(), std::forward<Args>(args));
+	}
 
 private:
 	std::vector<TAlignedMemoryUniquePtr<std::byte>> m_blocks;
+
 	std::size_t m_blockSizeInBytes;
 	std::size_t m_currentBlockIdx;
-	std::byte* m_currentBlockPtr;
-	std::size_t m_currentByteIdxInBlock;
+	std::byte*  m_blockPtr;
+	std::size_t m_remainingBytesInBlock;
 	std::size_t m_numUsedBytes;
 };
 
+inline std::size_t MemoryArena::numUsedBytes() const
+{
+	return m_numUsedBytes;
+}
 
+inline std::size_t MemoryArena::numAllocatedBytes() const
+{
+	return getBlockSizeInBytes() * numAllocatedBlocks();
+}
+
+inline std::size_t MemoryArena::getBlockSizeInBytes() const
+{
+	return m_blockSizeInBytes;
+}
+
+inline std::size_t MemoryArena::numAllocatedBlocks() const
+{
+	return m_blocks.size();
+}
 
 }// end namespace ph
