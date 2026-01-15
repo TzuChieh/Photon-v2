c11cf53e7d97ec02b48055fd6bf28758e1eb201f
Tzu-Chieh Chang <r05922176@ntu.edu.tw>
2026-01-15 21:04:04 +0800

    remove unnecessary cd hack in unit tests

--- Engine/EngineDeepTest/Source/main.cpp
+++ Engine/EngineDeepTest/Source/main.cpp
@@ -3,18 +3,8 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include <iostream>
-#include <filesystem>
-
 int main(int argc, char* argv[])
 {
-	// HACK: cd to expected working directory so Photon can properly init (test adapter in
-	// VS 17.10.1 is discovering tests in wrong working directory)
-	if(std::filesystem::current_path().filename() == "bin")
-	{
-		std::filesystem::current_path(std::filesystem::current_path().parent_path());
-	}
-
 	// Since Google Mock depends on Google Test, InitGoogleMock() is
 	// also responsible for initializing Google Test. Therefore there's
 	// no need for calling testing::InitGoogleTest() separately.
@@ -22,7 +12,6 @@ int main(int argc, char* argv[])
 
 	if(!ph::init_render_engine())
 	{
-		std::cerr << "Photon initialization failed" << std::endl;
 		return EXIT_FAILURE;
 	}
 
@@ -30,7 +19,6 @@ int main(int argc, char* argv[])
 
 	if(!ph::exit_render_engine())
 	{
-		std::cerr << "Photon exiting failed" << std::endl;
 		return EXIT_FAILURE;
 	}
 
